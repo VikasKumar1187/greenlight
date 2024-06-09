7466ea4d5ab73da2a4675d40bc7d391a61588a78
VikasKumar1187 <vikasdhaka.nitk@gmail.com>
2024-06-09 11:49:50 +0530

    6

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -39,3 +39,21 @@ func (app *application) writeJson(w http.ResponseWriter, status int, data any, h
 
 	return nil
 }
+
+func (app *application) exampleHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"hello": "world",
+	}
+	// Set the "Content-Type: application/json" header on the response.
+	w.Header().Set("Content-Type", "application/json")
+	// Use the json.NewEncoder() function to initialize a json.Encoder instance that
+	// writes to the http.ResponseWriter. Then we call its Encode() method, passing in
+	// the data that we want to encode to JSON (which in this case is the map above). If
+	// the data can be successfully encoded to JSON, it will then be written to our
+	// http.ResponseWriter.
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
+}
