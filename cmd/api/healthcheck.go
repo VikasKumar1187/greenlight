21381fa429cecc51106b6992ee1706b7caefd036
VikasKumar1187 <vikasdhaka.nitk@gmail.com>
2024-06-09 09:40:51 +0530

    5

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,41 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	healthCkeckData := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	// get the json representation of the healthcheck data in bytes slice
+	err := app.writeJson(w, http.StatusOK, healthCkeckData, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "Internal Server Error : The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+}
+
+func (app *application) writeJson(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	js = append(js, '\n')
+
 	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(js))
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
